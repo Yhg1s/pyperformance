9317d35a3c419ada164d4d923105b6e69fba575e
Thomas Wouters <thomas@python.org>
2026-08-24 14:40:23 +0200

    Disable <redacted> pyperf logic.

--- pyperformance/venv.py
+++ pyperformance/venv.py
@@ -256,13 +256,13 @@ class VenvForBenchmarks(_venv.VirtualEnvironment):
             requirements = Requirements.from_benchmarks([bench])
 
         # Every benchmark must depend on pyperf.
-        if bench is not None and not requirements.get("pyperf"):
-            basereqs = Requirements.from_file(REQUIREMENTS_FILE)
-            pyperf_req = basereqs.get("pyperf")
-            if not pyperf_req:
-                raise NotImplementedError
-            requirements.specs.append(pyperf_req)
-            # XXX what about psutil?
+        #if bench is not None and not requirements.get("pyperf"):
+        #    basereqs = Requirements.from_file(REQUIREMENTS_FILE)
+        #    pyperf_req = basereqs.get("pyperf")
+        #    if not pyperf_req:
+        #        raise NotImplementedError
+        #    requirements.specs.append(pyperf_req)
+        #    # XXX what about psutil?
 
         if not requirements:
             print("(nothing to install)")
